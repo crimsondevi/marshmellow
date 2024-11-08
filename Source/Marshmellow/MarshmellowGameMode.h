d9b2641f5e59139a453f43912f8aec5091b0cc38
Markus <markus@riddlebit.net>
2024-11-08 20:11:43 +0100

    Create Marshmellow Project

---
+++ Source/Marshmellow/MarshmellowGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MarshmellowGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMarshmellowGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMarshmellowGameMode();
+};
+
+
+
